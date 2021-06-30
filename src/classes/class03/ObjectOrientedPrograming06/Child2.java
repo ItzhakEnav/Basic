f20574d0d26f8ced39315127ef7b4ac3ce147c86
Itzhak Enav <60060148+ItzhakEnav@users.noreply.github.com>
2021-06-30 22:30:58 +0300

    doing control + alt + l

--- src/classes/class03/ObjectOrientedPrograming06/Child2.java
+++ src/classes/class03/ObjectOrientedPrograming06/Child2.java
@@ -1,5 +1,5 @@
 package classes.class03.ObjectOrientedPrograming06;
 
-public class  Child2 extends Family  {
-    String sisName = familyName+" "+"chia";
+public class Child2 extends Family {
+    String sisName = familyName + " " + "chia";
 }
