a57dfcdcae60b8825099d96c6d79e7233a89cb9d
Itzhak Enav <60060148+ItzhakEnav@users.noreply.github.com>
2021-06-30 20:00:36 +0300

    change the Model reading from constructors and chek if the numbers of screen Height and screen Width is positive

--- src/classes/class03/ObjectOrientedPrograming05/MainProgram.java
+++ src/classes/class03/ObjectOrientedPrograming05/MainProgram.java
@@ -2,15 +2,14 @@ package classes.class03.ObjectOrientedPrograming05;
 
 public class MainProgram {
     public static void main(String[] args) {
-        MobileDevice MD = new MobileDevice();
+        MobileDevice MD = new MobileDevice( 10,6);
         MD.hasFlash = true;
         MD.model = "Iphone";
         MD.os = 'i';
         MD.OSVersion = 8.12;
         MD.price = 4330;
         MD.printParameters();
-        MD.screenHeight = 22;
-        MD.screenWidth = 47;
+
         System.out.println("Area: " + MD.calculateArea());
         MD.pictureQuality();
     }
--- src/classes/class03/ObjectOrientedPrograming05/MobileDevice.java
+++ src/classes/class03/ObjectOrientedPrograming05/MobileDevice.java
@@ -9,6 +9,16 @@ public class MobileDevice {
     int screenWidth;
     int screenHeight;
 
+    MobileDevice(int screenWidth, int screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        if (this.screenHeight >= 0) {
+            System.out.println("The number is a positive");
+        } else {
+            System.out.println(" the number is not positive");
+        }
+    }
+
     void printParameters() {
         System.out.println("model: " + model + "\n" + "os: " + os + "\n" + "OS Version: " + OSVersion + "\n" + "hasFlash: " + hasFlash + "\n" + "price: " + price);
     }
@@ -18,10 +28,9 @@ public class MobileDevice {
     }
 
     void pictureQuality() {
-        if(hasFlash){
+        if (hasFlash) {
             System.out.println("Good Quality");
-        }
-        else {
+        } else {
             System.out.println("Bad Quality");
         }
     }
