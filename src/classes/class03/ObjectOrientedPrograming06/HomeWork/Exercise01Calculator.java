f2a942f6c1743af22e9d5b4f846f75e4010ff764
Itzhak Enav <60060148+ItzhakEnav@users.noreply.github.com>
2021-07-14 18:08:16 +0300

    finishing part 1 Ex1 with class Exercise01Calculator (main) of home work ObjectOrientedPrograming06

--- src/classes/class03/ObjectOrientedPrograming06/HomeWork/Exercise01Calculator.java
+++ src/classes/class03/ObjectOrientedPrograming06/HomeWork/Exercise01Calculator.java
@@ -1,5 +1,38 @@
 package classes.class03.ObjectOrientedPrograming06.HomeWork;
 
-public class Exercise01Calculator {
 
+import java.util.Scanner;
+
+public class Exercise01Calculator {
+    static Scanner in = new Scanner(System.in);  // Create a Scanner object
+
+    public static void main(String[] args) {
+        int Numbers = in.nextInt();// Enter the user input to Numbers
+        String arry[] = {"22.7", "*", "4"};
+        Calculator(arry);
+    }
+
+    public static void Calculator(String[] arry) {
+        switch (arry[1]) {
+            case "+":
+                System.out.println(Double.parseDouble(arry[0]) + Double.parseDouble(arry[2]));
+                break;
+            case "-":
+                System.out.println(Double.parseDouble(arry[0]) - Double.parseDouble(arry[2]));
+                break;
+            case "*":
+                System.out.println(Double.parseDouble(arry[0]) * Double.parseDouble(arry[2]));
+                break;
+            case "/":
+                System.out.println(Double.parseDouble(arry[0]) / Double.parseDouble(arry[2]));
+                break;
+            default:
+                System.out.println("input not valid");
+                break;
+        }
+    }
+    public static String NumbersToWords(int Numbers){
+
+        return null;
+    }
 }
